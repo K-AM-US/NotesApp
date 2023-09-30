937adba549d7e0ff225b905e18bcbaf538ed306c
mauriciocasillas <kamusgallagher@gmail.com>
2023-09-30 13:46:24 -0600

    Hiding Keyboard Extension

--- NotesApp/Extensions/HidingKeyboardExtension.swift
+++ NotesApp/Extensions/HidingKeyboardExtension.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+}
